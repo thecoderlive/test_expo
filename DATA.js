26c96e329aceb2d30be0e2a0acd0f980eb3a0689
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-30 16:01:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "321c3eca-5064-4425-a54d-bdb74bd6092f",
+    "home_feed": [
+        {
+            "id": "0bdc4d8e-10f6-44b2-b31a-9419a5abf4b7",
+            "artist_name": "production",
+            "home_carousel": [
+                {
+                    "id": "48d38902-1db1-4d62-b7b1-4d7862dcca5a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "3cfa2fa9-9b57-4bad-9e45-997697a1d438",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png"
+                },
+                {
+                    "id": "d63d1f31-325c-41fd-9d88-c6a350cd0ad9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "7b40af76-1e41-40ac-a12f-5af95f2550ad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png"
+                },
+                {
+                    "id": "6acc8406-024f-40a9-aaa4-66c16ac8b38f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                }
+            ],
+            "art_caption": "art",
+            "art_description": "Food first player. Identify year possible teacher."
+        },
+        {
+            "id": "64d2530e-419a-4a6c-a306-42acc378705f",
+            "artist_name": "either",
+            "home_carousel": [
+                {
+                    "id": "dc47f22e-8f28-4517-8365-26a7c01c0c93",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "ef65a776-71fb-4580-94a2-57d9773e02ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "8234219d-8712-439a-a96f-0f4da6a7627c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "c5ec9733-5167-47bd-b9dc-c454f21dd4d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "0d7bc697-b8c5-44d0-b870-65d7c11e1166",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "soldier",
+            "art_description": "Year upon involve recent indeed reality. Play law knowledge role heavy red weight. Fine data our sell. Turn walk when time decision whom."
+        },
+        {
+            "id": "4abc5e3d-71a2-494d-acad-d80083715bd3",
+            "artist_name": "civil",
+            "home_carousel": [
+                {
+                    "id": "a794de05-28ef-467c-bcb6-6eecd441d834",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png"
+                },
+                {
+                    "id": "ecc6abb5-86d1-423c-8067-9705de355072",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                },
+                {
+                    "id": "e33d0c2a-eb28-483e-82f6-91d6ab9a3537",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "5302142e-3f63-499e-ac4c-523636a579dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "b7fd9b67-77fa-49bd-b99c-e83eadc1210f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                }
+            ],
+            "art_caption": "when",
+            "art_description": "Easy well point short low quality. Main music evidence wonder political. Give point real friend."
+        },
+        {
+            "id": "a26c3c83-6a67-45f6-8231-c2152cdbb5ad",
+            "artist_name": "dream",
+            "home_carousel": [
+                {
+                    "id": "e64227ec-4aeb-4d27-923a-0a2568136159",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                },
+                {
+                    "id": "5c404836-f5a0-4250-927c-236831e4b1bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+                },
+                {
+                    "id": "8d06d5fb-8da8-4612-9d0c-e00ce8dee323",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+                },
+                {
+                    "id": "37aba56e-9bf3-42c8-9845-f9f6cc06a9f9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "089afb1b-8cea-477d-8c29-3957a2f3b9d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png"
+                }
+            ],
+            "art_caption": "oil",
+            "art_description": "Or current very live realize. Congress at change draw someone."
+        },
+        {
+            "id": "5442158d-edfc-4289-9c96-461767d684c2",
+            "artist_name": "then",
+            "home_carousel": [
+                {
+                    "id": "08b214ce-8e08-47af-b2dc-71029534ffc8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png"
+                },
+                {
+                    "id": "9c87e0e6-0046-4cd3-bc28-b5a346cb2e57",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "a5beaa67-44e9-48d6-bc92-bc8d37579c6a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "5daf1dfe-3d72-4c70-9656-5434f29ed2e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "7e45c525-5739-4688-b613-f2a75fb89c2a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                }
+            ],
+            "art_caption": "color",
+            "art_description": "Travel mission bar piece whole inside them ahead."
+        }
+    ]
+}
